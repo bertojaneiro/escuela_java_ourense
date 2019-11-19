8ea77a870d3638439c6d29597c12d2d3cdf280ac
Alberto <bertojaneiro@gmail.com>
2019-11-19 17:55:23 +0100

    jquery

--- 03_HTML5/jsjQuery/10_jQueryDOM.js
+++ 03_HTML5/jsjQuery/10_jQueryDOM.js
@@ -1,1 +1,16 @@
+//jQuery se abrebia como $(...)
+jQuery(document).ready(function(){
+    jQuery("section").append("<div><p>Leyenda de lo que sea</p></div>");
+    $("tr").append("<td>Info</td>");
+    $(".articulo").append("<p>UN PARRAFO EN CADA CLASS ARTICULO</p>");
+    $("td:nth-child(5)").attr("style","background-color:red; color: white;");
+    $("td:nth-child(5)").click(()=>{
+        alert("un click desde jQ");
+    });
+    $(".articulo").slideUp();
+    $("#otro_menu").click(function(){
+        $(this).slideUp();
+        $(".articulo").slideDown();
 
+    });
+});
